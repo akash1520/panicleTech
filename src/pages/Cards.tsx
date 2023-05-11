501ecdf840af0aa28cd02935792dc32319654939
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-05-12 03:09:10 +0530

    Feat (Cards.tsx) : Added data not found condition

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -7,7 +7,7 @@ import { RootState } from '../reducers';
 export default function Cards() {
     const users = useSelector((state: RootState) => state.users);
   return (
-    <>{
+    <>{ users===null?(<h3>Data Not found!!</h3>):(
         <Grid id="cards" container spacing={4} sx={{ px: "3rem" }}>
           {users.map((user) => (
             <Grid item xs={6} md={4} key={user.id}>
@@ -15,7 +15,7 @@ export default function Cards() {
             </Grid>
 
           ))}
-        </Grid>
+        </Grid>)
       }</>
   )
 }
